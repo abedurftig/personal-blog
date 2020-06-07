ea87b339f99bfe35f451864648107d6180d639e6
abedurftig <dasnervtdoch@googlemail.com>
2020-06-07 11:45:05 +0200

    Linting

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,25 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
       { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png', href: '/apple-touch-icon.png' }
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        type: 'image/png',
+        href: '/apple-touch-icon.png'
+      }
     ]
   },
   /*
