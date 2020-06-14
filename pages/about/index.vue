bc0f002af6d68caacb2cb1bf0ee18ea21db6ed08
abedurftig <dasnervtdoch@googlemail.com>
2020-06-14 21:44:15 +0200

    Improve columns on tablet

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="columns is-mobile is-gapless">
-    <div class="column is-hidden-mobile"></div>
+    <div class="column is-hidden-mobile is-hidden-tablet"></div>
     <div class="column">
       <About>
         <nuxt-content :document="page" />
       </About>
     </div>
-    <div class="column is-hidden-mobile"></div>
+    <div class="column is-hidden-mobile is-hidden-tablet"></div>
   </div>
 </template>
 <script>
